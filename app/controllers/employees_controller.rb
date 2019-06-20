9893bc84398eaac9ca4139ff4771f86ae5bc196a
Carolina <carolinaevevarela@gmail.com>
2019-06-19 21:15:03 -0400

    Pegada en el formulario

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,18 @@
 class EmployeesController < ApplicationController
+  def index
+    @employee = Employee.all
+  end
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+    redirect_to @employee.company
+  end
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to @employee.company
+  end
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :company_id)
+  end
 end
