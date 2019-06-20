ee413a12969816b813a3ea482139283de7d20f86
Carolina <carolinaevevarela@gmail.com>
2019-06-19 21:45:26 -0400

    Listando index

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,6 +13,6 @@ class EmployeesController < ApplicationController
     redirect_to @employee.company
   end
   def employee_params
-    params.require(:employee).permit(:first_name, :last_name, :email, :company_id)
+    params.require(:employee).permit(:first_name, :last_name, :email, :area_id, :company_id)
   end
 end
